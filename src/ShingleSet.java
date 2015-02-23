fe5743b14820c86df0f3250d74c57cd5dfe28943
Tom Harting <t.harting@student.tudelft.nl>
2015-02-23 19:04:37 +0100

    Initial commit

---
+++ src/ShingleSet.java
@@ -0,0 +1,57 @@
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+
+/**
+ * The ShingleSet class contains a sorted set of shingles.
+ * @author hansgaiser
+ *
+ */
+public class ShingleSet extends TreeSet<String> implements SortedSet<String> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2143400328259342668L;
+	
+	/**
+	 * The size of the shingles.
+	 */
+	private int k;
+	
+	/**
+	 * Constructor for the ShingleSet.
+	 * @param k The size of the shingles.
+	 */
+	public ShingleSet(int k) {
+		this.k = k;
+	}
+	
+	/**
+	 * Add shingles of size k to the set from String s.
+	 * @param s The string that is to be transformed to shingles.
+	 */
+	public void shingleString(String s) {
+		// ADD CODE HERE
+	}
+	
+	/**
+	 * Add shingles of size k to the set from String s, where all white spaces from s are removed.
+	 * @param s The string that is to be transformed to shingles.
+	 */
+	public void shingleStrippedString(String s) {
+		// ADD CODE HERE
+	}
+	
+	/**
+	 * Calculates the Jaccard distance between this set and the other set.
+	 * @param other The other set of shingles that this set will be compared to.
+	 * @return The Jaccard distance between this set and the other set.
+	 */
+	public double jaccardDistance(TreeSet<String> other) {
+		// ADD CODE HERE
+		
+		return 0.0; // remove when code is completed
+	}
+
+}
---
+++ src/main.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.Set;
+
+
+public class main {
+	
+	public static void exercise1_1() {
+		// ADD CODE HERE
+	}
+	
+	public static void exercise1_2() {
+		// ADD CODE HERE
+		
+		//exercise1_3(mh);
+	}
+	
+	public static void exercise1_3(MinHash mh) {
+		// ADD CODE HERE
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// exercise 1.1
+		exercise1_1();
+		
+		// exercise 1.2
+		exercise1_2();
+	}
+
+}
