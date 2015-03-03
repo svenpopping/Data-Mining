a27f7619339534da7271325d656ade0215bcac3e
svenpopping <s.popping@student.tudelft.nl>
2015-03-03 14:38:36 +0100

    NearestNeighbour dingen enz

--- src/NearestNeighbour.java
+++ src/NearestNeighbour.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 
@@ -44,9 +40,37 @@ public class NearestNeighbour {
 	public int predict(List<Double> features, int k) {
 		// the result
 		int label = -1;
-		
-		// add code here
 
-		return label;
+		// add code here
+        Map<Double, FeatureVector> distances = new HashMap<Double, FeatureVector>(); // Sort them by distance, smallest distances first.
+
+        for (int i = 0; i < this.getDataset().size(); i++) { //foreach element in the dataset.
+            double distance = this.getDataset().get(i).distance(features); // Calculate the distance
+
+            distances.put(distance, this.getDataset().get(i));
+        }
+
+        System.out.println(distances.keySet());
+
+        FeatureVector[] values = (FeatureVector[]) distances.values().toArray();
+        FeatureVector[] nearestNeighbours = new FeatureVector[k];
+        for (int i = 0; i < k; i++) {
+            nearestNeighbours[i] = values[i];
+        }
+
+        int one = 0;
+        int two = 0;
+        for (int i = 0; i < nearestNeighbours.length; i++) {
+            if (nearestNeighbours[i].getLabel() == 1)
+                one ++;
+            else if (nearestNeighbours[i].getLabel() == -1)
+                two++;
+        }
+
+        label = (one > two) ? 1 : -1;
+        // Select k of the nearest objects.
+        // Calculate which label has the highest majority vote.
+        // Assign the label to that class.
+        return label;
 	}
 }
