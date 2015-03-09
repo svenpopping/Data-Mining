acda0087d274b299733a322e6c483d12eec49163
Tom Harting <t.harting@student.tudelft.nl>
2015-03-09 16:04:33 +0100

    Added assignment 3

---
+++ Assignment 2/src/main.java
@@ -0,0 +1,75 @@
+
+public class main {
+
+	public static void perceptron() {
+		Perceptron p1 = new Perceptron(1);
+        Dataset d1 = new Dataset("data/gaussian.txt", true);
+        PerceptronPlotter pp1 = new PerceptronPlotter("1", "-1");
+
+        p1.updateWeights(d1);
+
+        pp1.plotData(d1, p1);
+        // Equation for decision line is y = -3x
+	}
+
+	public static void perceptronDigits() {
+		Dataset d2 = new Dataset("data/train_digits.txt", true);
+        DigitFrame df1 = new DigitFrame("train_digits 1");
+        DigitFrame df2 = new DigitFrame("train_digits 0");
+
+        df1.showImage(d2.get(50), 8, 8);
+        df2.showImage(d2.get(49), 8, 8);
+        // Amount of zeros = 50, Amount of ones 50
+
+        Perceptron p2 = new Perceptron(1);
+        p2.updateWeights(d2);
+        p2.updateWeights(d2);
+
+        DigitFrame df3 = new DigitFrame("weights");
+        df3.showImage(p2.getWeights(), 8, 8);
+
+        DigitFrame df4 = new DigitFrame("Wrong predicts");
+        Dataset d3 = new Dataset("data/test_digits.txt", true);
+        double error = 0.0;
+        for (int i = 0; i < d3.size(); i++) {
+            if (d3.get(i).getLabel() != p2.predict(d3.get(i))){
+                error++;
+                df4.showImage(d3.get(i), 8, 8);
+            }
+        }
+        error = error/d3.size();
+        System.out.println(error);
+    }
+
+	public static void nearestNeighbour() {
+		NearestNeighbour nearestNeighbour = new NearestNeighbour();
+        nearestNeighbour.readData("data/banana.txt");
+
+        NearestNeighbourPlotter nearestNeighbourPlotter = new NearestNeighbourPlotter(3);
+        nearestNeighbourPlotter.plotData(nearestNeighbour);
+	}
+	
+	public static void nearestNeighbourDigits() {
+        NearestNeighbour nearestNeighbour2 = new NearestNeighbour();
+        nearestNeighbour2.readData("data/test_digits.txt");
+
+        Dataset d4 = new Dataset("data/test_digits.txt",true);
+
+        double error = 0.0;
+        for (int i = 0; i < d4.size(); i++) {
+            if (d4.get(i).getLabel() != nearestNeighbour2.predict(d4.get(i), 3)){
+                error++;
+            }
+        }
+        error = error/d4.size();
+        System.out.println(error);
+    }
+
+	public static void main(String[] args) {
+//		perceptron();
+//		perceptronDigits();
+//		nearestNeighbour();
+		nearestNeighbourDigits();
+	}
+
+}
--- src/main.java
+++ src/main.java
@@ -1,75 +1,45 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 
 public class main {
 
-	public static void perceptron() {
-		Perceptron p1 = new Perceptron(1);
-        Dataset d1 = new Dataset("data/gaussian.txt", true);
-        PerceptronPlotter pp1 = new PerceptronPlotter("1", "-1");
-
-        p1.updateWeights(d1);
-
-        pp1.plotData(d1, p1);
-        // Equation for decision line is y = -3x
-	}
-
-	public static void perceptronDigits() {
-		Dataset d2 = new Dataset("data/train_digits.txt", true);
-        DigitFrame df1 = new DigitFrame("train_digits 1");
-        DigitFrame df2 = new DigitFrame("train_digits 0");
-
-        df1.showImage(d2.get(50), 8, 8);
-        df2.showImage(d2.get(49), 8, 8);
-        // Amount of zeros = 50, Amount of ones 50
-
-        Perceptron p2 = new Perceptron(1);
-        p2.updateWeights(d2);
-        p2.updateWeights(d2);
-
-        DigitFrame df3 = new DigitFrame("weights");
-        df3.showImage(p2.getWeights(), 8, 8);
-
-        DigitFrame df4 = new DigitFrame("Wrong predicts");
-        Dataset d3 = new Dataset("data/test_digits.txt", true);
-        double error = 0.0;
-        for (int i = 0; i < d3.size(); i++) {
-            if (d3.get(i).getLabel() != p2.predict(d3.get(i))){
-                error++;
-                df4.showImage(d3.get(i), 8, 8);
-            }
-        }
-        error = error/d3.size();
-        System.out.println(error);
-    }
-
-	public static void nearestNeighbour() {
-		NearestNeighbour nearestNeighbour = new NearestNeighbour();
-        nearestNeighbour.readData("data/banana.txt");
-
-        NearestNeighbourPlotter nearestNeighbourPlotter = new NearestNeighbourPlotter(3);
-        nearestNeighbourPlotter.plotData(nearestNeighbour);
-	}
-	
-	public static void nearestNeighbourDigits() {
-        NearestNeighbour nearestNeighbour2 = new NearestNeighbour();
-        nearestNeighbour2.readData("data/test_digits.txt");
-
-        Dataset d4 = new Dataset("data/test_digits.txt",true);
-
-        double error = 0.0;
-        for (int i = 0; i < d4.size(); i++) {
-            if (d4.get(i).getLabel() != nearestNeighbour2.predict(d4.get(i), 3)){
-                error++;
-            }
-        }
-        error = error/d4.size();
-        System.out.println(error);
-    }
-
 	public static void main(String[] args) {
-//		perceptron();
-		perceptronDigits();
-//		nearestNeighbour();
-//		nearestNeighbourDigits();
+
+        // FILL IN YOUR CODE HERE
+
+	}
+
+	/*
+	 * Java method to sort Map in Java by value e.g. HashMap or Hashtable
+	 * throw NullPointerException if Map contains null values
+	 * It also sort values even if they are duplicates
+	 */
+	public static <K extends Comparable,V extends Comparable> Map<K,V> sortByValues(Map<K,V> map){
+		List<Map.Entry<K,V>> entries = new LinkedList<Map.Entry<K,V>>(map.entrySet());
+
+		Collections.sort(entries, new Comparator<Map.Entry<K,V>>() {
+
+			@Override
+			public int compare(Entry<K, V> o1, Entry<K, V> o2) {
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+
+		//LinkedHashMap will keep the keys in the order they are inserted
+		//which is currently sorted on natural ordering
+		Map<K,V> sortedMap = new LinkedHashMap<K,V>();
+
+		for(Map.Entry<K,V> entry: entries){
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+
+		return sortedMap;
 	}
 
 }
